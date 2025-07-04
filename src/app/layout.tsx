b4dcc4a3e865e99d5b6b1c655ae14a71974b4ad2
coolcmyk <ryanadi001@gmail.com>
2025-07-04 15:19:33 +0700

    dev: forcing dark-mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,7 +69,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="icon" href="/logo-preview.png" sizes="any" />
@@ -82,7 +82,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="dark"
           enableSystem={false}
         >
           <main className="flex min-h-screen flex-col">
